c8fd9b38ccd04a11c2206fdc40aff702b9b8ab04
dezue <mistfoll@mail.ru>
2026-02-25 15:58:04 +0700

    added class animal

---
+++ lab02-sem2-oop/Animal.cs
@@ -0,0 +1,23 @@
+﻿namespace Animal
+{
+  public class Animal
+    {
+    public string name;
+    public int age;
+    public string habitat;
+    public string dietType;
+
+    public Animal(string animalName, int animalAge, string animalHabitat, string animalDietType)
+    {
+      name = animalName;
+      age = animalAge;
+      habitat = animalHabitat;
+      dietType = animalDietType;
+    }
+
+        public virtual string GetInfo()
+        {
+            return $"Name: {name}, Age: {age}, Habitat: {habitat}, Diet: {dietType}";
+        }
+    }
+}
