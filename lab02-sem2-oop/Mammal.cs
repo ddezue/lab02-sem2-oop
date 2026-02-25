34b4aeeb8060fcfee91a79e597bc10ea22baaeb3
dezue <mistfoll@mail.ru>
2026-02-25 16:15:48 +0700

    added mammal class

---
+++ lab02-sem2-oop/Mammal.cs
@@ -0,0 +1,19 @@
+﻿namespace Mammal
+{
+  public class Mammal : Animal
+  {
+    public bool hasFur;
+
+    public Mammal(string animalName, int animalAge, string animalHabitat, string animalDietType, bool animalHasFur)
+      : base(animalName, animalAge, animalHabitat, animalDietType)
+    {
+      hasFur = animalHasFur;
+    }
+
+    public override string GetInfo()
+    {
+      string furStat = hasFur ? "yes" : "no";
+      return base.GetInfo() + $", Type: Mammal, Fur: {furStat}";
+    }
+  }
+}
